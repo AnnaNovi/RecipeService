459aed8adcd35ebaf418f3ffb2be8d2a40e9219e
Anna <annprogram-gh@mail.ru>
2022-07-01 17:15:16 +0300

    finished with pageNotFound and pageUnderConstruction

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,31 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/pageNotFound/page-not-found/page-not-found.component';
+import { PageUnderConsrtructionComponent } from './pages/pageUnderConstruction/page-under-consrtruction/page-under-consrtruction.component';
 import { RecipeComponent } from './pages/recipe/recipe.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    component: HomeComponent
+    component: HomeComponent,
   },
   {
     path: 'recipe',
-    component: RecipeComponent
+    component: RecipeComponent,
+  },
+  {
+    path: 'categories',
+    component: PageUnderConsrtructionComponent,
+  },
+  {
+    path: 'recipe/:category',
+    component: PageUnderConsrtructionComponent,
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
   },
 ];
 
---
+++ src/app/pages/pageUnderConstruction/page-under-consrtruction/page-under-consrtruction.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-under-consrtruction',
+  templateUrl: './page-under-consrtruction.component.html',
+  styleUrls: ['./page-under-consrtruction.component.scss']
+})
+export class PageUnderConsrtructionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
