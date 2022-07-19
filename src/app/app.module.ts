2d986cb2bf98e58e3b3c022038a677767df4a66a
Anna <annprogram-gh@mail.ru>
2022-07-19 17:47:49 +0300

    add FormGroup for filters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
 
@@ -48,6 +48,7 @@ import { LoopForNumberDirective } from './directives/loopForNumber/loop-for-numb
     HttpClientModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     SwiperModule,
   ],
   providers: [],
