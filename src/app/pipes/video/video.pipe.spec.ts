e89f1f08a7cf295f3ab8fc1d06ea61ca4cdbb2fa
Anna <annprogram-gh@mail.ru>
2022-07-30 19:13:20 +0300

    add tests for VideoPipe

--- src/app/pipes/video/video.pipe.spec.ts
+++ src/app/pipes/video/video.pipe.spec.ts
@@ -1,8 +1,30 @@
-/* import { VideoPipe } from './video.pipe';
+import { TestBed } from '@angular/core/testing';
+import { DomSanitizer } from '@angular/platform-browser';
+import { VideoPipe } from './video.pipe';
 
 describe('VideoPipe', () => {
+  let pipe: VideoPipe;
+  let sanitizer: DomSanitizer;
+
+  beforeEach(() => {
+    sanitizer = TestBed.get(DomSanitizer);
+    pipe = new VideoPipe(sanitizer);
+  })
+
   it('create an instance', () => {
-    const pipe = new VideoPipe();
     expect(pipe).toBeTruthy();
   });
-}); */
+  it("should return input videoURL if it isn't include 'www.youtube.com'", () => {
+    const url = 'https://cookieandkate.com/best-hummus-recipe/';
+    const result = pipe.transform(url);
+    expect(result).toBe(result);
+  });
+  it("should transform input videoURL if it is include 'www.youtube.com'", () => {
+    const url = 'https://www.youtube.com/watch?v=1ahpSTf_Pvk';
+    const resultBySanitizer = sanitizer.bypassSecurityTrustResourceUrl(
+      'https://www.youtube.com/embed/1ahpSTf_Pvk'
+    );
+    const result = pipe.transform(url);
+    expect(result).toEqual(resultBySanitizer);
+  });
+});
