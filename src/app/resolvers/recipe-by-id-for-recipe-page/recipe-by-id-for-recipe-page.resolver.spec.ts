a2ceb7cfc6be8bf775a53472e57953f270e30160
Anna <annprogram-gh@mail.ru>
2022-08-03 19:17:00 +0300

    added sprite.svg and code refactored

---
+++ src/app/resolvers/recipe-by-id-for-recipe-page/recipe-by-id-for-recipe-page.resolver.spec.ts
@@ -0,0 +1,17 @@
+/* import { TestBed } from '@angular/core/testing';
+
+import { RecipeByIdResolver } from './recipe-by-id.resolver';
+
+describe('RecipeByIdResolver', () => {
+  let resolver: RecipeByIdResolver;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    resolver = TestBed.inject(RecipeByIdResolver);
+  });
+
+  it('should be created', () => {
+    expect(resolver).toBeTruthy();
+  });
+});
+ */
---
+++ src/app/resolvers/recipe-by-id-for-recipe-page/recipe-by-id-for-recipe-page.resolver.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {
+  Router, Resolve,
+  RouterStateSnapshot,
+  ActivatedRouteSnapshot
+} from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { RecipeByIdService } from 'src/app/services/recipe-by-id/recipe-by-id.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipeByIdForRecipePageResolver implements Resolve<boolean> {
+  constructor(private recipeByIdService: RecipeByIdService) {}
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    const id = route.paramMap.get('id');
+    return this.recipeByIdService.getRecipeById(id);
+  }
+}
