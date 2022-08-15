f2e9ef4efbdf1069b32044b0faf352cdfbeb2561
Anna <annprogram-gh@mail.ru>
2022-08-15 21:55:30 +0300

    working with FavoritesComponent

---
+++ src/app/pages/favorites/favorites.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject, map, Subscription } from 'rxjs';
+import { recipePreview } from 'src/app/models';
+import { FavoritesService } from 'src/app/services/favorites/favorites.service';
+
+@Component({
+  selector: 'app-favorites',
+  templateUrl: './favorites.component.html',
+  styleUrls: ['./favorites.component.scss'],
+})
+export class FavoritesComponent implements OnInit, OnDestroy {
+  public recipesPreviewList$: BehaviorSubject<recipePreview[] | null> =
+    this.favoritesService.favoritesRecipesList$;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private favoritesService: FavoritesService
+  ) {}
+
+  private setRecipesPreviewList(): Subscription {
+    return this.activatedRoute.data
+      .pipe(
+        map((data) => {
+          this.favoritesService.favoritesRecipesList$.next(data['recipesList']);
+        })
+      )
+      .subscribe();
+  }
+
+  ngOnInit(): void {
+    this.setRecipesPreviewList();
+  }
+  ngOnDestroy(): void {
+    this.setRecipesPreviewList().unsubscribe();
+  }
+}
