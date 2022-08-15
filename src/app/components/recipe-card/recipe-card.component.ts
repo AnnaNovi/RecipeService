a989484b596bf66ae98ae5e422772951254b8b3a
Anna <annprogram-gh@mail.ru>
2022-08-15 21:52:35 +0300

    working with FavoritesComponent

--- src/app/components/recipe-card/recipe-card.component.ts
+++ src/app/components/recipe-card/recipe-card.component.ts
@@ -1,14 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { recipePreview } from 'src/app/models';
+import { FavoritesService } from 'src/app/services/favorites/favorites.service';
 
 @Component({
   selector: 'app-recipe-card',
   templateUrl: './recipe-card.component.html',
   styleUrls: ['./recipe-card.component.scss'],
 })
-export class RecipeCardComponent {
-  
+export class RecipeCardComponent implements OnInit {
+  isFavorite: boolean = false;
   @Input() recipe!: recipePreview;
 
+  constructor(private favoritesService: FavoritesService) {}
+
+  public toggleFavorite(id: string) {
+    this.favoritesService.toggleFavorite(id);
+    this.isFavorite = !this.isFavorite;
+  }
+
+  ngOnInit(): void {
+    this.isFavorite = this.favoritesService.isFavoriteRecipe(this.recipe.id);
+  }
 }
